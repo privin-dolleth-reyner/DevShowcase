dd6a156e55b8853527ab49675500ab2129b81472
privindolleth <privindolleth3@gmail.com>
2025-01-22 10:37:26 +0530

    Platform based module setup

--- data/currency-converter/src/wasmJsMain/kotlin/com/cmp/showcase/data/currency/converter/di/HttpClientModule.wasmJs.kt
+++ data/currency-converter/src/wasmJsMain/kotlin/com/cmp/showcase/data/currency/converter/di/HttpClientModule.wasmJs.kt
@@ -1,8 +1,8 @@
 package com.cmp.showcase.data.currency.converter.di
 
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.js.JsClient
+import io.ktor.client.engine.js.Js
 
 actual fun getHttpClient(): HttpClient {
-    return HttpClient()
+    return HttpClient(Js)
 }
--- features/currency-converter/src/commonMain/kotlin/com/cmp/showcase/features/currency/converter/di/AppModule.kt
+++ features/currency-converter/src/commonMain/kotlin/com/cmp/showcase/features/currency/converter/di/AppModule.kt
@@ -1,12 +1,13 @@
 package com.cmp.showcase.features.currency.converter.di
 
-import com.cmp.showcase.features.currency.converter.CurrencyConverterViewModel
 import org.koin.core.KoinApplication
-import org.koin.core.module.dsl.viewModelOf
+import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val featureModule = module {
-    viewModelOf(::CurrencyConverterViewModel)
+
 }
 
-fun KoinApplication.currencyFeatureModule() = arrayOf(featureModule)
+expect val platformModule: Module
+
+fun KoinApplication.currencyFeatureModule() = arrayOf(featureModule, platformModule)
